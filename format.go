6c726b39dd4f7a4321f689b68d98720505de0140
Nicolas Leiva <nleiva@cisco.com>
2017-02-27 17:00:08 -0500

    Added Indent and Compact wrappers for JSON files

---
+++ format.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	//Option to read from static string
+	//b := []byte(`{"Cisco-IOS-XR-ipv4-bgp-cfg:bgp":"empty"}`)
+
+	//Option to read from file
+	b, err := ioutil.ReadFile("files/bgp.json") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	//Add indentation
+	b, _ = prettyprint(b)
+
+	//Remove indentation
+	b, _ = unprettyprint(b)
+
+	//Print one of the two optiona
+	fmt.Printf("%s", b)
+}
+
+func prettyprint(b []byte) ([]byte, error) {
+	var out bytes.Buffer
+	err := json.Indent(&out, b, "", "  ")
+	return out.Bytes(), err
+}
+
+func unprettyprint(b []byte) ([]byte, error) {
+	var out bytes.Buffer
+	err := json.Compact(&out, b)
+	return out.Bytes(), err
+}
